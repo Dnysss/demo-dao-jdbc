f3314daba85892944b2a12905750e10f13149d60
Dnysss <denysantos2014@hotmail.com>
2024-05-01 11:08:16 -0300

    Seller entity class

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,12 +1,15 @@
 package app;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Seller seller = new Seller(21, "Denys", "denys@gmail.com", new Date(), 3000.0, new Department(1, "Books"));
+		System.out.println(seller);
 	}
 
 }
