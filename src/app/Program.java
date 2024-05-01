678ccdb95c4d36045125a5681b83796fa618c109
Dnysss <denysantos2014@hotmail.com>
2024-05-01 11:55:42 -0300

    SellerDaoJDBC and DaoFactory

--- src/app/Program.java
+++ src/app/Program.java
@@ -2,6 +2,8 @@ package app;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,7 +11,11 @@ public class Program {
 
 	public static void main(String[] args) {
 		Seller seller = new Seller(21, "Denys", "denys@gmail.com", new Date(), 3000.0, new Department(1, "Books"));
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println(seller);
+		
+		
 	}
 
 }
