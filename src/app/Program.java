0f098d3eaa10c5b2744d308d2493f603243b8ea4
Dnysss <denysantos2014@hotmail.com>
2024-05-01 16:34:18 -0300

    findById implementation

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,21 +1,16 @@
 package app;
 
-import java.util.Date;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Seller seller = new Seller(21, "Denys", "denys@gmail.com", new Date(), 3000.0, new Department(1, "Books"));
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
-		
-		
 	}
-
 }
--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -9,8 +9,8 @@ public class Department implements Serializable {
 	private Integer id;
 	private String name;
 	
-	@SuppressWarnings("unused")
-	private Department() {
+	
+	public Department() {
 	}
 
 	public Department(Integer id, String name) {
