87c772f03bc8e15df3bee9c458e0c503c2e784e7
Dnysss <denysantos2014@hotmail.com>
2024-05-01 11:32:58 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,21 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Department;
+
+public class DepartmentDao {
+	void insert(Department obj) {
+		
+	}
+	void update(Department obj) {
+	}
+	void deleteById(Department obg) {
+	}
+	Department findById(Integer id) {
+		return null;
+	}
+	List<Department> findall() {
+		return null;
+	}
+}
---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,20 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Seller;
+
+public class SellerDao {
+	void insert(Seller obj) {
+	}
+	void update(Seller obj) {
+	}
+	void deleteById(Seller obg) {
+	}
+	Seller findById(Integer id) {
+		return null;
+	}
+	List<Seller> findall() {
+		return null;
+	}
+}
